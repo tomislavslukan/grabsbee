a912d2f27a6c6e3b7445fec72f9e9a11485c3b1c
tomislavslukan <tomislav.slukan@inchoo.net>
2024-02-16 08:24:35 +0100

    Styleguide

--- design/frontend/Inchoo/grabsbee/web/tailwind/tailwind.config.js
+++ design/frontend/Inchoo/grabsbee/web/tailwind/tailwind.config.js
@@ -8,7 +8,165 @@ const hyvaModules = require('@hyva-themes/hyva-modules');
 
 module.exports = hyvaModules.mergeTailwindConfig({
   theme: {
-    extend: {}
+    extend: {
+        colors : {
+            primary: {
+                DEFAULT: "#12167A",
+                light: "#12167a",
+                dark: "#10146e",
+            },
+            secondary: {
+                DEFAULT: "#EC1D2E",
+                light: "#ee3443",
+                dark: "#d41a29",
+                90: "#D00505",
+            },
+            gray: {
+                DEFAULT: "#7D7D7D",
+                90: "#696969",
+                60: "#828282",
+                25: "#9C9C9C",
+                10: "#CFCFCF",
+                5: "#E8E8E8",
+            },
+            text: {
+                dark: "#1A1A1A",
+                color: "#4E4E4E",
+                invert: "#FFFFFF",
+                muted: "#CFCFCF",
+            },
+            link: {
+                visited: "#B9121D",
+                active: "#740D15",
+            },
+            white: {
+                DEFAULT: "#fff",
+            },
+            success: {
+                DEFAULT: "#32D08D",
+            },
+            error: {
+                DEFAULT: "#F14F39",
+            },
+            warning: {
+                DEFAULT: "#F1B239",
+            },
+        },
+
+        borderWidth: {
+            "1px": '1px',
+        },
+
+        screens: {
+            'mobile': '360px',
+            'desktop': '1024px',
+        },
+
+        fontFamily: {
+            'primary': ['Open Sans', 'sans-serif'],
+            'heading': ['Anton', 'sans-serif'],
+        },
+
+        spacing: {
+            'h6': '0.375rem',
+            'h5': '0.75rem',
+            'h4': '1.5rem',
+            'h3': '1.875rem',
+            'h2': '2.25rem',
+            'h1': '3rem',
+        },
+
+        fontSize: {
+
+            // desktop
+            'h1-d': [
+                '1.75rem',
+                {
+                    lineHeight: '3rem',
+                }
+            ],
+            'h2-d': [
+                '1.875rem',
+                {
+                    lineHeight: '3rem',
+                }
+            ],
+            'h3-d': [
+                '1.375rem',
+                {
+                    lineHeight: '1.875rem',
+                }
+            ],
+            'h4-d': [
+                '1.25rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+            'h5-d': [
+                '1.063rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+            'h6-d': [
+                '1rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+            'p-d': [
+                '1rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+
+            // mobile
+            'h1-m': [
+                '1.5rem',
+                {
+                    lineHeight: '2.5rem',
+                }
+            ],
+            'h2-m': [
+                '1.25rem',
+                {
+                    lineHeight: '2.5rem',
+                }
+            ],
+            'h3-m': [
+                '1rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+            'h4-m': [
+                '0.938rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+            'h5-m': [
+                '0.875rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+            'h6-m': [
+                '0.813rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+            'p-m': [
+                '0.875rem',
+                {
+                    lineHeight: '1.5rem',
+                }
+            ],
+        }
+    }
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
   // Examples for excluding patterns from purge
--- etc/config.php
+++ etc/config.php
@@ -334,7 +334,6 @@ return [
         'Hyva_HoTemplatehints' => 1,
         'Hyva_Theme' => 1,
         'Inchoo_StyleGuide' => 1,
-        'Inchoo_Test' => 1,
         'MarkShust_LayoutHandleLogger' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
